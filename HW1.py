878ef0028928c1da039977db928bb4aeca9f00f3
Chieh622 <willy98172@gmail.com>
2023-09-17 17:23:31 +0800

    Chieh

--- HW1.py
+++ HW1.py
@@ -2,10 +2,9 @@ n = int(input('Enter number:'))
 
 def fib(n):
     fib = [0, 1]
-    if n == 0:
-        return 0
-    if n == 1:
-        return 1
+    if n <  0: raise
+    if n == 0: return 0
+    if n == 1: return 1
 
     for i in range (2, n+1):
             fib.append(fib[i-1] + fib[i-2])
