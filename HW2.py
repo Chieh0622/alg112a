25de78d742a6ff021d5aa79953845e239c1180c1
Chieh622 <willy98172@gmail.com>
2023-10-04 09:35:13 +0800

    Chieh

--- HW2.py
+++ HW2.py
@@ -1,3 +1,5 @@
+
+
 # 方法 1
 def power2n(n):
     return 2**n
@@ -5,23 +7,29 @@ def power2n(n):
 # 方法 2a：用遞迴
 # power2n(n-1)+power2n(n-1)
 def power2n_a(n):
-    if n <  0: return False
     if n == 0: return 1
-    if n == 1: return 2
-    return power2n_a(n-1) + power2n_a(n-1)
+    return power2n_a(n - 1) + power2n_a(n - 1)
 
 # 方法2b：用遞迴
 # 2*power2n(n-1)
 def power2n_b(n):
-    if n <  0: return False
     if n == 0: return 1
-    if n == 1: return 2
     return 2 * power2n_b(n-1)
 
-
-# 方法 3：用遞迴+查表
+#方法 3：用遞迴+查表
 # if ....
-# power2n(n-1)+power2n(n-1) 
-def power2n_3(n):
+# power2n(n-1)+power2n(n-1)
+fib = [None] * 10000
+fib[0] = 1
+def power2n_c(n):
     if n <  0: return False
-    if n == 0: return 
+    if not fib[n] is None: return fib[n]
+    fib[n] = power2n_c(n-1) + power2n_c(n-1)
+    return fib[n]
+
+n = int(input('Please enter a number: '))
+
+print(f'power2n({n})   = {power2n(n)}')
+print(f'power2n_a({n}) = {power2n_a(n)}')
+print(f'power2n_b({n}) = {power2n_b(n)}')
+print(f'power2n_c({n}) = {power2n_c(n)}')
---
+++ HW4/Brute.py
@@ -0,0 +1,6 @@
+# x^2 - 3x + 1 = 0
+
+from numpy import arange
+import math
+
+# 1.Brute
