a3cf7c82cfe8e8ae206dd9448b1a81b9a5426a09
Chieh622 <willy98172@gmail.com>
2024-01-04 22:32:38 +0800

    Chieh

--- HW3/truthtable.py
+++ HW3/truthtable.py
@@ -2,7 +2,7 @@ def truthtable(n, current_table=[]):
     if n == 0:
         print(current_table) 
     else:
-        for value in [0, 1, 2]:
+        for value in [0, 1]:
             truthtable(n - 1, current_table + [value])
 
 truthtable(int(input("enter number : ")))
